5c958633bfe9fcdeaa24943ab73e50958d35b24b
Will Luna <wluna@anaconda.com>
2023-11-10 23:58:28 -0800

    initial data prep complete

--- code/data_prep.py
+++ code/data_prep.py
@@ -3,19 +3,19 @@ import glob
 
 def df_to_dialogue(df, speaker_col, dialoge_col, media_name):
     df['speaker'] = df[speaker_col]
-    df['dialoge'] = df[dialoge_col]
+    df['dialogue'] = df[dialoge_col]
     df['media'] = media_name
 
-    df = df[['speaker', 'dialoge', 'media']]
+    df = df[['speaker', 'dialogue', 'media']]
     return df
 
 def concat_dfs(*dfs):
     df = pd.concat(dfs, ignore_index=True)
     return df
 
-def prepare_friends_data():
+def prepare_txt_data(file_path):
 
-    files = glob.glob('../data/friends/*.txt')
+    files = glob.glob(file_path)
 
     all_lines = []
 
@@ -27,24 +27,62 @@ def prepare_friends_data():
 
     all_lines = [line.split(':') for line in all_lines]
     all_lines = [line for line in all_lines if len(line) == 2]
-    all_lines = [line for line in all_lines if line[0] in ['Joey', 'Chandler', 'Monica', 'Rachel', 'Ross', 'Phoebe']]
 
     df = pd.DataFrame(all_lines, columns=['speaker', 'dialogue'])
+    return df
+
+def prepare_unstructured_data(file_path):
+
+    files = glob.glob(file_path)
+
+    all_lines = []
+
+    for file in files:
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+    
+    #remove the newline character from the end of each line
+    all_lines = [line.strip() for line in all_lines]
+
+    all_dialogue = []
+
+    for i in range(len(all_lines)):
+
+        #evaluate whether the line is a speaker or dialogue
+        if all_lines[i].isupper() and i < len(all_lines):
+            #add the current line and the next line to the dialogue list as a tuple
+            all_dialogue.append((all_lines[i], all_lines[i+1]))
+
+    #convert the dialogue list to a dataframe
+    df = pd.DataFrame(all_dialogue, columns=['speaker', 'dialogue'])
+
     return df
 
 def prepare_data():
     simpson = pd.read_csv('../data/simpsons/simpsons_dataset.csv')
     rickmorty = pd.read_csv('../data/rickandmorty/RickAndMortyScripts.csv')
     lotr = pd.read_csv('../data/lotr/lotr_scripts.csv')
-    friends = prepare_friends_data()
+    friends = prepare_txt_data('../data/friends/*.txt')
+    prideprejudice = prepare_txt_data('../data/prideprejudice/*.txt')
+    downtownabbey = prepare_unstructured_data('../data/downtownabbey/*.txt')
 
     df = concat_dfs(
             df_to_dialogue(rickmorty, 'name', 'line', 'Rick and Morty'),
             df_to_dialogue(lotr, 'char', 'dialog', 'The Lord of the Rings'),
             df_to_dialogue(simpson, 'raw_character_text', 'spoken_words', 'The Simpsons'),
-            df_to_dialogue(friends, 'speaker', 'dialogue', 'Friends')
+            df_to_dialogue(friends, 'speaker', 'dialogue', 'Friends'),
+            df_to_dialogue(prideprejudice, 'speaker', 'dialogue', 'Pride and Prejudice'),
+            df_to_dialogue(downtownabbey, 'speaker', 'dialogue', 'Downtown Abbey')
         )
-    return df
+    
+    cleaned_df = df.dropna()
+    #trim quotation marks and spaces from the dialogue
+    #cleaned_df['dialogue'] = cleaned_df['dialogue'].str.strip('\"')
+    
+    return cleaned_df
 
 df = prepare_data()
 print(df.groupby('media')['speaker'].value_counts().groupby(level=0).head(5))
+#save the dataframe to a csv file
+df.to_csv('../data/dialogue.csv', index=False)
