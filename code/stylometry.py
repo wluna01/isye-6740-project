df76b5c99ef3ca156c5bcccd48aa86f6fcad9523
Will Luna <will.luna01@outlook.com>
2023-11-29 18:12:18 -0800

    sentence length distributions up and running

--- code/stylometry.py
+++ code/stylometry.py
@@ -18,22 +18,42 @@ for media in dialogue['media'].unique():
     #identify the top 6 speakers in the media_df
     top_speakers = media_df['speaker'].value_counts().index.tolist()[:6]
     #for the top speakers in the media_df
+    plt.cla()
+    plt.xlabel('Sentence Length (words)')
+    plt.ylabel('Density')
     for speaker in top_speakers:
-        #calculate the average length of each sentence spoken by the speaker
+        #filter the media_df to only include the speaker
         speaker_df = media_df[media_df['speaker'] == speaker]
         #convert the dialogue column to a list of sentences
         sentences = speaker_df['dialogue'].tolist()
         #use the get_sentence_lengths function to calculate the sentence length distribution
         sentence_lengths = get_sentence_lengths(sentences)
-        #print(speaker)
-        #print(length_distribution)
+        #plot the distribution
+        sns.kdeplot(sentence_lengths, bw_adjust=0.8, label=speaker)
         
-        #print(speaker_df['dialogue'].head())
-        # Plotting the distribution as a KDE curve
-        sns.kdeplot(sentence_lengths, bw_adjust=0.5)
-        plt.xlabel('Sentence Length (words)')
-        plt.ylabel('Density')
-        #set the title to the speaker's name
-        plt.title(speaker)
-        #save the plot to a png file
-        plt.savefig(f'../images/{speaker}_sentence_length.png')
+    plt.title(media)
+    plt.legend(title='Speaker')
+    plt.savefig(f'../images/{media}_sentence_length.png')
+
+#do the same as above, comparing across media
+plt.cla()
+plt.xlabel('Sentence Length (words)')
+plt.ylabel('Density')
+
+for media in dialogue['media'].unique():
+    #create a df with only the rows where the media column is equal to the current media value
+    media_df = dialogue[dialogue['media'] == media]
+    #identify the top 6 speakers in the media_df
+    top_speakers = media_df['speaker'].value_counts().index.tolist()[:6]
+    #filter the media_df to only include the top speakers
+    media_df = media_df[media_df['speaker'].isin(top_speakers)]
+    sentences = media_df['dialogue'].tolist()
+    #use the get_sentence_lengths function to calculate the sentence length distribution
+    sentence_lengths = get_sentence_lengths(sentences)
+    #plot the distribution
+    sns.kdeplot(sentence_lengths, bw_adjust=1.2, label=media)
+
+plt.legend(title='Media')
+#restrict x axis to be between 0 and 40
+plt.xlim(0, 30)
+plt.savefig(f'../images/overall_sentence_length.png')
